6158e9e09f6fb4334d6e014536ec86b09f68242d
harleigh <harleigh.marsh@gmail.com>
2023-10-13 08:19:55 -0700

    added calculator files

---
+++ src/GlutenCalculator.js
@@ -0,0 +1,7 @@
+export function GlutenCalculator({ calcData }) {
+  return (
+    <div>
+      <h3>I am the C A L C U L A T O R of G L U T E N !!!</h3>
+    </div>
+  );
+}
--- src/HighGlutenCalcApp.js
+++ src/HighGlutenCalcApp.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { EntryForm } from "./EntryForm";
 import { initialValues } from "./calcData";
+import { GlutenCalculator } from "./GlutenCalculator";
 
 export default function HighGlutenCalcApp() {
+  //calcData has all of the input from the user to determine the high gluten flour amount
   const [calcData, setCalcData] = useState(initialValues);
   return (
     <div className="App">
       <h3>High Gluten Calculator</h3>
       <EntryForm calcData={calcData} setCalcData={setCalcData} />
+      <GlutenCalculator calcData={calcData} />
       {console.log(calcData)}
     </div>
   );
