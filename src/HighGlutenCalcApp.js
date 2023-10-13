05d6469c794c1d6d987f2e067bd58d203caca6cb
harleigh <harleigh.marsh@gmail.com>
2023-10-13 10:19:42 -0700

    made css class name camelCase

--- src/HighGlutenCalcApp.js
+++ src/HighGlutenCalcApp.js
@@ -7,7 +7,7 @@ export default function HighGlutenCalcApp() {
   //calcData has all of the input from the user to determine the high gluten flour amount
   const [calcData, setCalcData] = useState(initialValues);
   return (
-    <div className="App">
+    <div className="mainApp">
       <h3>High Gluten Calculator</h3>
       <EntryForm calcData={calcData} setCalcData={setCalcData} />
       <GlutenCalculator calcData={calcData} />
